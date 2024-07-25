8cef0ecca6c79b005cbb1162d39320340956a42a
Shubham <meetshubhamku@gmail.com>
2024-07-25 13:56:27 +0530

    added response headers

--- index.js
+++ index.js
@@ -9,25 +9,32 @@ const port = process.env.PORT || 3000;
 app.get("/unsafe", fetchImage, async (req, res) => {
   const { img, metadata } = req.profile;
   console.log("params : ", req.query);
+  // console.log("Metadata - ", metadata);
 
   const height = parseInt(req.query.height) || parseInt(metadata.height);
   const width = parseInt(req.query.width) || parseInt(metadata.width);
-  const quality = parseInt(req.query.quality || 80);
+  const quality = parseInt(req.query.quality || 70);
   const fit = req.query.fit || "cover";
   const format = req.query.format || "webp";
 
-  const data = await sharp(img)
+  await sharp(img)
     .resize({
       width,
       height,
       fit,
+      quality,
     })
-    .webp({
-      quality: quality,
+    .toFormat(format)
+    .toBuffer({ resolveWithObject: true })
+    .then(({ data, info }) => {
+      console.info("INFO - ", info);
+      res.setHeader("Content-Type", `image/${format}`).end(data);
     })
-    .toBuffer();
+
+    .catch((err) => {
+      return res.json(err).end();
+    });
   // res.type("image/webp");
-  res.end(data);
 });
 
 app.get("/", (req, res) => {
